733e0770a10f26cc6601c7817941da3741bbc348
SeHoonC <csh970605@gmail.com>
2024-08-30 14:34:45 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.py
@@ -0,0 +1,38 @@
+# problem statement : https://www.acmicpc.net/problem/2108
+
+import sys
+input = sys.stdin.readline
+
+alphadic = {
+    'a' : 0,
+    'b' : 1,
+    'c' : 2,
+    'd' : 3,
+    'e' : 4,
+    'f' : 5,
+    'g' : 6,
+    'h' : 7,
+    'i' : 8,
+    'j' : 9,
+    'k' : 10,
+    'l' : 11,
+    'm' : 12,
+    'n' : 13,
+    'o' : 14,
+    'p' : 15,
+    'q' : 16,
+    'r' : 17,
+    's' : 18,
+    't' : 19,
+    'u' : 20,
+    'v' : 21,
+    'w' : 22,
+    'x' : 23,
+    'y' : 24,
+    'z' : 25
+}
+ss = input().strip()
+arr = [0]*26
+for s in ss:
+    arr[alphadic[s]] +=1
+print(' '.join([str(x) for x in arr]))
