0b43a1d46d89779e4001ad6f667435679e4c4e24
SeHoonC <csh970605@gmail.com>
2024-08-30 14:24:55 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 132 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void solution() throws Exception {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        br.close();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                bw.write("*");
+            }
+            bw.write("\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+    public static void main(String[] args) throws Exception {
+        solution();
+    }
+}
