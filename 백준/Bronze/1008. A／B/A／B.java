057484745a71208de05b8032ce83200b70e855f7
SeHoonC <csh970605@gmail.com>
2024-08-30 11:11:29 +0900

    [Bronze V] Title: A/B, Time: 120 ms, Memory: 16120 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,23 @@
+import java.io.BufferedWriter;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void solution() throws Exception {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        double a = Double.parseDouble(st.nextToken());
+        double b = Double.parseDouble(st.nextToken());
+        bw.write(a / b + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
+    public static void main(String[] args) throws Exception {
+        solution();
+    }
+}
