2d5410deaddb6c479d242ee97a7dd379e433edf2
SeHoonC <csh970605@gmail.com>
2024-08-30 14:52:32 +0900

    [Bronze V] Title: 새싹, Time: 120 ms, Memory: 14100 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,19 @@
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void solution() throws Exception {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write("         ,r'\"7\n");
+        bw.write("r`-_   ,'  ,/\n");
+        bw.write(" \\. \". L_r'\n");
+        bw.write("   `~\\/\n");
+        bw.write("      |\n");
+        bw.write("      |\n");
+        bw.flush();
+        bw.close();
+    }
+    public static void main(String[] args) throws Exception {
+        solution();
+    }
+}
