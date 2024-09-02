7eff177d6bd67ed271e4621db80f2bd03abd70fb
SeHoonC <csh970605@gmail.com>
2024-09-02 12:28:09 +0900

    [Silver II] Title: 제곱수의 합, Time: 6612 ms, Memory: 31900 KB -BaekjoonHub

--- 백준/Silver/1699. 제곱수의 합/제곱수의 합.py
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.py
@@ -1,15 +1,13 @@
 # problem statement : https://www.acmicpc.net/problem/1699
 import sys
 input = sys.stdin.readline
-import math
 n = int(input())
-dp = [999999999999] * (n + 1)
+dp = [999999999999] * (n+1)
 dp[0] = 0
 
-for i in range(1, n + 1):
-    sqrt_i = int(math.sqrt(i))
+for i in range(1, n+1):
+    sqrt_i = int(i**0.5)
     for j in range(1, sqrt_i+1):
-        square = j * j
-        dp[i] = min(dp[i], dp[i-square]+1)
+        dp[i] = min(dp[i], dp[i-j**2]+1)
 
 print(dp[n])
