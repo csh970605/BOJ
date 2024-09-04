7e96a9eee581a3d7b6962b9bb68a95258e846e57
SeHoonC <csh970605@gmail.com>
2024-09-04 15:59:22 +0900

    [Gold V] Title: ABCDE, Time: 720 ms, Memory: 34200 KB -BaekjoonHub

---
+++ 백준/Gold/13023. ABCDE/ABCDE.py
@@ -0,0 +1,31 @@
+# problem statement : https://www.acmicpc.net/problem/13023
+import sys
+input = sys.stdin.readline
+from collections import defaultdict
+sys.setrecursionlimit(10000)
+
+def dfs(x, depth):
+    if depth == 4:
+        print(1)
+        exit()
+    visited[x] = 1
+    for neighbor in graph[x]:
+        if not visited[neighbor]:
+            dfs(neighbor, depth+1)
+    visited[x] = False
+
+n, m = map(int, input().split())
+graph = defaultdict(list)
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+visited = [0] * (n)
+
+count = 0
+for i in range(n):
+    dfs(i, 0)
+
+print(0)
