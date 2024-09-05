1e3ebbe377bd2c469974e7266450aa47d45aa7a4
SeHoonC <csh970605@gmail.com>
2024-09-05 14:21:33 +0900

    [Gold IV] Title: 이모티콘, Time: 76 ms, Memory: 39352 KB -BaekjoonHub

--- 백준/Gold/14226. 이모티콘/이모티콘.py
+++ 백준/Gold/14226. 이모티콘/이모티콘.py
@@ -16,12 +16,10 @@ def bfs():
             visited[num][num] = time + 1
             q.append((num, time+1, num))
 
-        # 2. 붙여넣기 (클립보드에 있는 이모티콘을 붙여넣기)
         if clip_board > 0 and num+clip_board <= n and visited[num+clip_board][clip_board] == -1:
             visited[num+clip_board][clip_board] = time + 1
             q.append((num+clip_board, time+1, clip_board))
 
-        # 3. 삭제 (이모티콘 1개 삭제)
         if num > 1 and visited[num-1][clip_board] == -1:
             visited[num-1][clip_board] = time + 1
             q.append((num-1, time+1, clip_board))
