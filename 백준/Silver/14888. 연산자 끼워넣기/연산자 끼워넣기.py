30219294088d162e502d75f4eb2bd055a11900ec
SeHoonC <csh970605@gmail.com>
2024-09-06 11:48:19 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,44 +1,36 @@
-# problem statement : https://www.acmicpc.net/problem/14888
+# problem statement : https://www.acmicpc.net/problem/15658
 import sys
 input = sys.stdin.readline
-from itertools import permutations
+
+def dfs(index, current, plus, minus, multi, div):
+    global max_value, min_value
+
+    if index == n:
+        max_value = max(max_value, current)
+        min_value = min(min_value, current)
+        return
+    
+    if plus:
+        dfs(index+1, current+arr[index], plus-1, minus, multi, div)
+    if minus:
+        dfs(index+1, current-arr[index], plus, minus-1, multi, div)
+    if multi:
+        dfs(index+1, current*arr[index], plus, minus, multi-1, div)
+    if div:
+        if current < 0:
+            dfs(index+1, -(-current//arr[index]), plus, minus, multi, div-1)
+        else:
+            dfs(index+1, current//arr[index], plus, minus, multi, div-1)
+            
+
 
 n = int(input())
 arr = list(map(int, input().split()))
 nums = list(map(int, input().split()))
-operators = []
-for i, num in enumerate(nums):
-    for j in range(num):
-        if i == 0:
-            operators.append('+')
-        elif i == 1:
-            operators.append('-')
-        elif i == 2:
-            operators.append('*')
-        elif i == 3:
-            operators.append('/')
-operator_permutations = set(permutations(operators))
-
 max_value = -float('inf')
 min_value = float('inf')
 
-for operator_seq in operator_permutations:
-    result = arr[0]
-    for i in range(1, n):
-        if operator_seq[i-1] == '+':
-            result += arr[i]
-        elif operator_seq[i-1] == '-':
-            result -= arr[i]
-        elif operator_seq[i-1] == '*':
-            result *= arr[i]
-        elif operator_seq[i-1] == '/':
-            if result < 0:
-                result = -(-result // arr[i])
-            else:
-                result //= arr[i]
-    
-    max_value = max(max_value, result)
-    min_value = min(min_value, result)
+dfs(1, arr[0], nums[0], nums[1], nums[2], nums[3])
 
 print(max_value)
 print(min_value)
